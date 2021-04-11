b1955d14153732b3d220f4271aad291d191d072f
Tarr Márton <tarr.marton1@gmail.com>
2021-04-11 13:47:47 +0200

    Added transitions

--- Projekt/BarnManager/src/main/notjava/example/controller/LoginController.java
+++ Projekt/BarnManager/src/main/notjava/example/controller/LoginController.java
@@ -2,27 +2,22 @@ package example.controller;
 
 import example.Database.JpaUserDAO;
 import example.Database.UserDAO;
-import example.model.GenderType;
 import example.model.User;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class LoginController {
+
+    @FXML
+    private AnchorPane rootPane;
 
     @FXML
     private ResourceBundle resources;
@@ -50,6 +45,8 @@ public class LoginController {
     @FXML
     void initialize() {
 
+        FadeController fadeController = new FadeController();
+
         loginButton.setOnAction(actionEvent -> { // when login button is clicked, get the values from the textboxes
             UserDAO uDaO = new JpaUserDAO();
             String username = userName.getText().trim();
@@ -66,35 +63,15 @@ public class LoginController {
             }
 
             if(!found) feedbackLabel.setVisible(true); //if user does not exist, provide an error message
-            else{ //change scene to mainWindow.fxml
-                Stage stage;
-                Parent root;
-
-                stage = (Stage)loginButton.getScene().getWindow();
-                try {
-                    root = FXMLLoader.load(getClass().getResource("/example/view/mainWindow.fxml"));
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            else{
+                fadeController.fadeOut("/example/view/mainWindow.fxml", rootPane);
             }
+
         });
 
         loginRegister.setOnAction(actionEvent -> { //when register button is clicked, change window to register.fxml
-            Stage stage;
-            Parent root;
-
-            stage = (Stage)loginRegister.getScene().getWindow();
-            try {
-                root = FXMLLoader.load(getClass().getResource("/example/view/register.fxml"));
-                Scene scene = new Scene(root);
-                stage.setScene(scene);
-                stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            fadeController.fadeOut("/example/view/register.fxml", rootPane);
         });
+
     }
 }
