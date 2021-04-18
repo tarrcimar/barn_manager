488c4bd072e0287f38e363384beab4fab17ea593
Tarr Márton <tarr.marton1@gmail.com>
2021-04-18 12:01:02 +0200

    Added examples for usage

--- Projekt/BarnManager/src/main/notjava/example/controller/MainWindowController.java
+++ Projekt/BarnManager/src/main/notjava/example/controller/MainWindowController.java
@@ -1,9 +1,14 @@
 package example.controller;
 
 
+import example.Database.DAO;
+import example.Database.JpaDAO;
+import example.model.Animal;
+import example.model.Barn;
 import javafx.fxml.FXML;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainWindowController {
@@ -24,7 +29,19 @@ public class MainWindowController {
     private URL location;
 
     @FXML
-    void initialize() {
+    void initialize() throws InterruptedException {
+
+        /* Példák a lekérdezésekre
+        DAO minden = new JpaDAO();
         System.out.println("In mainwindow: " + userId);
+        Barn b = minden.getBarnByUserId(userId);
+        System.out.println(b.getName());
+
+        List<Animal> animalList = minden.getAnimalsByBarnId(1);
+        System.out.println(animalList.size());
+        for (Animal a: animalList) {
+            System.out.println(a.getType() + " " + a.getId());
+        }
+         */
     }
 }
